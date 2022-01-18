3bddc9e41e35be6b257331f47677e304493bea08
Andrew Ferguson <andrew@fergcorp.com>
2022-01-18 14:20:42 -0800

    Appying black coding convensions

--- Source/code.py
+++ Source/code.py
@@ -1,6 +1,6 @@
-#TeachMe PCB Rev 2 Class
-#Bring up hardware, skull keypad - Jesse Robinson
-#11/8/2021
+# TeachMe PCB Rev 2 Class
+# Bring up hardware, skull keypad - Jesse Robinson
+# 11/8/2021
 
 import time
 import busio
@@ -17,25 +17,29 @@ from adafruit_hid.consumer_control_code import ConsumerControlCode
 import adafruit_veml7700
 import supervisor
 
-_TICKS_PERIOD = const(1<<29)
-_TICKS_MAX = const(_TICKS_PERIOD-1)
-_TICKS_HALFPERIOD = const(_TICKS_PERIOD//2)
+_TICKS_PERIOD = const(1 << 29)
+_TICKS_MAX = const(_TICKS_PERIOD - 1)
+_TICKS_HALFPERIOD = const(_TICKS_PERIOD // 2)
+
 
 def ticks_add(ticks, delta):
-#"Add a delta to a base number of ticks, performing wraparound at 2**29ms."
+    # "Add a delta to a base number of ticks, performing wraparound at 2**29ms."
     return (a + b) % _TICKS_PERIOD
 
+
 def ticks_diff(ticks1, ticks2):
-#"Compute the signed difference between two ticks values, assuming that they are␣
-#˓→within 2**28 ticks"
+    # "Compute the signed difference between two ticks values, assuming that they are␣
+    # ˓→within 2**28 ticks"
     diff = (ticks1 - ticks2) & _TICKS_MAX
     diff = ((diff + _TICKS_HALFPERIOD) & _TICKS_MAX) - _TICKS_HALFPERIOD
     return diff
 
+
 def ticks_less(ticks1, ticks2):
-#"Return true iff ticks1 is less than ticks2, assuming that they are within␣
-#˓→2**28 ticks"
+    # "Return true iff ticks1 is less than ticks2, assuming that they are within␣
+    # ˓→2**28 ticks"
     return ticks_diff(ticks1, ticks2) < 0
+
 
 lastUpdateTime = 0
 currentTime = 0
@@ -43,10 +47,10 @@ currentTime = 0
 i2c = busio.I2C(board.GP21, board.GP20)
 
 # Update this to match the number of NeoPixel LEDs connected to your board.
-num_pixels = 10 #was 10, 12 for encoder lights
+num_pixels = 10  # was 10, 12 for encoder lights
 
 brightnessSteps = 50
-pixelBrightness = brightnessSteps/2
+pixelBrightness = brightnessSteps / 2
 
 pixels = neopixel.NeoPixel(board.GP19, num_pixels)
 pixels.brightness = float(pixelBrightness) / brightnessSteps
@@ -93,11 +97,10 @@ keymap = {
     (7): (KEY, [Keycode.EIGHT]),
     (8): (KEY, [Keycode.NINE]),
     (9): (KEY, [Keycode.ZERO]),
-    (10): (KEY, [Keycode.X]), #unused
-    (11): (KEY, [Keycode.Y]), #unused
-   }
-switches = [0, 1, 2, 3, 4, 5, 6,
-            7, 8, 9, 10, 11]
+    (10): (KEY, [Keycode.X]),  # unused
+    (11): (KEY, [Keycode.Y]),  # unused
+}
+switches = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
 for i in range(10):
     switches[i] = DigitalInOut(pins[i])
@@ -118,8 +121,8 @@ rightEncoder_last_position = rightEncoder.position
 while not spi.try_lock():
     pass
 
-#while not i2c.try_lock():
-    #pass
+# while not i2c.try_lock():
+# pass
 
 veml7700 = adafruit_veml7700.VEML7700(i2c)
 
@@ -127,20 +130,21 @@ print("Ambient light:", veml7700.light)
 
 trackColor = 0
 
+
 def ledChange():
     global trackColor
 
-    if (trackColor == 0):
+    if trackColor == 0:
         pixels.fill(RED)
-    elif (trackColor == 1):
+    elif trackColor == 1:
         pixels.fill(YELLOW)
-    elif (trackColor == 2):
+    elif trackColor == 2:
         pixels.fill(GREEN)
-    elif (trackColor == 3):
+    elif trackColor == 3:
         pixels.fill(CYAN)
-    elif (trackColor == 4):
+    elif trackColor == 4:
         pixels.fill(BLUE)
-    elif (trackColor == 5):
+    elif trackColor == 5:
         pixels.fill(PURPLE)
     else:
         trackColor = 0
@@ -153,10 +157,11 @@ latch.value = True
 latch.value = False
 time.sleep(1)
 
-#order RGB
-leftEye = [1,0,0]
-rightEye = [0,1,1]
+# order RGB
+leftEye = [1, 0, 0]
+rightEye = [0, 1, 1]
 eyeWriteByte = 0
+
 
 def eyeUpdate():
     global leftEye
@@ -171,39 +176,47 @@ def eyeUpdate():
     rightEyeGreen = not rightEye[1]
     rightEyeBlue = not rightEye[2]
 
-    eyeWriteByte = (leftEyeRed << 0) | (leftEyeGreen << 1) | (leftEyeBlue << 2) | (rightEyeRed << 3) | (rightEyeGreen << 4) | (rightEyeBlue << 5)
+    eyeWriteByte = (
+        (leftEyeRed << 0)
+        | (leftEyeGreen << 1)
+        | (leftEyeBlue << 2)
+        | (rightEyeRed << 3)
+        | (rightEyeGreen << 4)
+        | (rightEyeBlue << 5)
+    )
     spi.write(bytes([eyeWriteByte]))
     latch.value = False
     latch.value = True
-    #print("EyeWrite: ", eyeWriteByte)
+    # print("EyeWrite: ", eyeWriteByte)
+
 
 ledChange()
 eyeUpdate()
 
 useUSB = False
 
-#if you want to use without USB (for lights and such) your HID calls will hang, pressing right encoder upon bootup disables USB calls to prevent this
-if (not switches[10].value):
+# if you want to use without USB (for lights and such) your HID calls will hang, pressing right encoder upon bootup disables USB calls to prevent this
+if not switches[10].value:
     useUSB = True
 
-if (useUSB == True):
-	kbd = Keyboard(usb_hid.devices)
-	cc = ConsumerControl(usb_hid.devices)
+if useUSB == True:
+    kbd = Keyboard(usb_hid.devices)
+    cc = ConsumerControl(usb_hid.devices)
 
 while True:
 
     ambientLight = veml7700.light
-    #print("Ambient light:", ambientLight)
+    # print("Ambient light:", ambientLight)
 
     for button in range(10):
         if switch_state[button] == 0:
             if not switches[button].value:
                 try:
                     if keymap[button][0] == KEY:
-                        if (useUSB == True):
+                        if useUSB == True:
                             kbd.press(*keymap[button][1])
                     else:
-                        if (useUSB == True):
+                        if useUSB == True:
                             cc.send(keymap[button][1])
                 except ValueError:  # deals w six key limit
                     pass
@@ -213,7 +226,7 @@ while True:
             if switches[button].value:
                 try:
                     if keymap[button][0] == KEY:
-                        if (useUSB == True):
+                        if useUSB == True:
                             kbd.release(*keymap[button][1])
                 except ValueError:
                     pass
@@ -227,10 +240,10 @@ while True:
                     pass
                     trackColor = trackColor + 1
                     ledChange()
-                    #kbd.press(*keymap[button][1])
+                    # kbd.press(*keymap[button][1])
                 else:
                     pass
-                    #cc.send(keymap[button][1])
+                    # cc.send(keymap[button][1])
             except ValueError:  # deals w six key limit
                 pass
             switch_state[button] = 1
@@ -240,7 +253,7 @@ while True:
             try:
                 if keymap[button][0] == KEY:
                     pass
-                    #kbd.release(*keymap[button][1])
+                    # kbd.release(*keymap[button][1])
             except ValueError:
                 pass
             switch_state[button] = 0
@@ -251,12 +264,12 @@ while True:
             try:
                 if keymap[button][0] == KEY:
                     pass
-                    if (useUSB == True):
+                    if useUSB == True:
                         cc.send(ConsumerControlCode.PLAY_PAUSE)
-                    #kbd.press(*keymap[button][1])
+                    # kbd.press(*keymap[button][1])
                 else:
                     pass
-                    #cc.send(keymap[button][1])
+                    # cc.send(keymap[button][1])
             except ValueError:  # deals w six key limit
                 pass
             switch_state[button] = 1
@@ -266,7 +279,7 @@ while True:
             try:
                 if keymap[button][0] == KEY:
                     pass
-                    #kbd.release(*keymap[button][1])
+                    # kbd.release(*keymap[button][1])
             except ValueError:
                 pass
             switch_state[button] = 0
@@ -275,15 +288,15 @@ while True:
     position_change = current_position - leftEncoder_last_position
     if position_change > 0:
         for _ in range(position_change):
-            if (pixelBrightness < brightnessSteps):
+            if pixelBrightness < brightnessSteps:
                 pixelBrightness = pixelBrightness + 1
-        #print(pixelBrightness)
+        # print(pixelBrightness)
         pixels.brightness = float(pixelBrightness) / brightnessSteps
     elif position_change < 0:
         for _ in range(-position_change):
-            if (pixelBrightness > 0):
+            if pixelBrightness > 0:
                 pixelBrightness = pixelBrightness - 1
-        #print(pixelBrightness)
+        # print(pixelBrightness)
         pixels.brightness = float(pixelBrightness) / brightnessSteps
     leftEncoder_last_position = current_position
 
@@ -291,14 +304,14 @@ while True:
     position_change = current_position - rightEncoder_last_position
     if position_change > 0:
         for _ in range(position_change):
-            if (useUSB == True):
+            if useUSB == True:
                 cc.send(ConsumerControlCode.VOLUME_INCREMENT)
-        #print(current_position)
+        # print(current_position)
     elif position_change < 0:
         for _ in range(-position_change):
-            if (useUSB == True):
+            if useUSB == True:
                 cc.send(ConsumerControlCode.VOLUME_DECREMENT)
-        #print(current_position)
+        # print(current_position)
     rightEncoder_last_position = current_position
 
     time.sleep(0.010)  # debounce
