485d3824626b6a242ec87701d0147b09d1d8186b
Andrew Ferguson <andrew@fergcorp.com>
2022-01-18 14:44:13 -0800

    Remove unused code

--- Source/code.py
+++ Source/code.py
@@ -15,31 +15,6 @@ from adafruit_hid.keycode import Keycode
 from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 import adafruit_veml7700
-import supervisor
-
-_TICKS_PERIOD = (1 << 29)
-_TICKS_MAX = (_TICKS_PERIOD - 1)
-_TICKS_HALFPERIOD = (_TICKS_PERIOD // 2)
-
-
-def ticks_add(ticks, delta):
-    # "Add a delta to a base number of ticks, performing wraparound at 2**29ms."
-    return (ticks + delta) % _TICKS_PERIOD
-
-
-def ticks_diff(ticks1, ticks2):
-    # "Compute the signed difference between two ticks values, assuming that they are␣
-    # ˓→within 2**28 ticks"
-    diff = (ticks1 - ticks2) & _TICKS_MAX
-    diff = ((diff + _TICKS_HALFPERIOD) & _TICKS_MAX) - _TICKS_HALFPERIOD
-    return diff
-
-
-def ticks_less(ticks1, ticks2):
-    # "Return true iff ticks1 is less than ticks2, assuming that they are within␣
-    # ˓→2**28 ticks"
-    return ticks_diff(ticks1, ticks2) < 0
-
 
 lastUpdateTime = 0
 currentTime = 0
